6edf96b1fe0f4743f38ccc08fa0c7995f851f2f0
Леонид Хабибуллин <leonidhabibullin@MacBook-Air.local>
2020-10-12 16:28:23 +0300

    сделал день первый

---
+++ day1.playground/Contents.swift
@@ -0,0 +1,35 @@
+import UIKit
+
+var str = "Hello, playground" // var задаёт переменные
+str = "Goodbye" // Swift сам может определять тип переменной
+
+var age = 38 // int
+var population = 8_000_000 // int
+
+var str1 = """
+this goes
+over multiple
+lines
+""" // multi-line str (на выводе будет в множество строк)
+
+var str2 = """
+This goes \
+over multiple \
+lines
+"""  // на выводе будет в одну строку
+print (str1)
+print (str2)
+
+var pi = 3.141 // double
+var awesome = true // bool
+
+var score = 85
+var str3 = "Your score is \(score)"
+print (str3)
+var results = "The test results are here: \(str3)"
+print(results)
+
+let taylor = "swift" // let задаёт константы
+
+let albim: String = "Reputation" // можно задавать тип переменной или константы явно через :
+
