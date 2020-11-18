652bd93c4b20dfaf2186130b1111ac33b08ba897
Leonkh <62510482+Leonkh@users.noreply.github.com>
2020-11-18 18:06:46 +0300

    Написал meme генератор с использованием Core Graphics, UIImagePickerController и UIAcrivityController

--- Projects/MilestoneProjectsFrom25to27/MilestoneProjectsFrom25to27/ViewController.swift
+++ Projects/MilestoneProjectsFrom25to27/MilestoneProjectsFrom25to27/ViewController.swift
@@ -159,6 +159,5 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         let vc = UIActivityViewController(activityItems: [image], applicationActivities: [])
         present(vc, animated: true)
     }
-
 }
 
