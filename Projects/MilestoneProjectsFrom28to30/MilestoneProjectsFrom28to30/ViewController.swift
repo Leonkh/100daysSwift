7d6fe18df6da4cc97f8e9a309be9e09692b7e869
Leonkh <62510482+Leonkh@users.noreply.github.com>
2020-11-20 00:52:55 +0300

    Оптимизация кода

---
+++ Projects/MilestoneProjectsFrom28to30/MilestoneProjectsFrom28to30/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MilestoneProjectsFrom28to30
+//
+//  Created by Леонид Хабибуллин on 19.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
